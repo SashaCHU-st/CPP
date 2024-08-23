ce2f26cee272d3b564978973f9053110efc74e82
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2024-08-23 10:54:51 +0300

    starting ex01

--- M01/ex01/main.cpp
+++ M01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 10:31:22 by aheinane          #+#    #+#             */
-/*   Updated: 2024/08/23 10:31:23 by aheinane         ###   ########.fr       */
+/*   Updated: 2024/08/23 10:54:15 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,26 +31,36 @@ void randomChump( std::string name )
 	new_zombie.announce();
 }
 
-Zombie* newZombie(std::string p_name)
-{
-	Zombie* name = new Zombie(p_name);
-	return(name);
-}
+// Zombie* newZombie(std::string p_name)
+// {
+// 	Zombie* name = new Zombie(p_name);
+// 	return(name);
+// }
 
 void Zombie::announce(void)
 {
 	cout << this->name <<"BraiiiiiiinnnzzzZ..." << endl;
 }
-
-int main()
+Zombie* zombieHorde( int N, std::string p_name )
 {
+	Zombie* name = new Zombie(p_name);
+	return(name);
+}
+
+int main(int argc, char **argv)
+{
+	int N = 5;
 	Zombie zombie("first ");/// creating object and giving anme how is it
 	zombie.announce(); // just name it
 
-	Zombie* nZombie = newZombie("second on heap "); /// calling function that creates on heap
-	nZombie->announce();// heap zombie tells his name
+	for(int i = 0; i < N; i++)
+	{
+		cout << "\n"<< " index "<< i ; 
+		Zombie* nZombie = zombieHorde(N," on heap "); /// calling function that creates on heap
+		nZombie->announce();// heap zombie tells his name
+		delete nZombie;
+	}
 	randomChump("third "); // creating new zombie and announcing them 
-	delete nZombie;
 
 	return(0);
 }
