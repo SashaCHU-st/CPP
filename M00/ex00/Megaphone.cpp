30ca3504938dff6fefe9da9c87fce6101d310bce
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2024-09-02 18:23:11 +0300

    starting m02

--- M00/ex00/Megaphone.cpp
+++ M00/ex00/Megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 16:45:22 by aheinane          #+#    #+#             */
-/*   Updated: 2024/08/16 09:39:29 by aheinane         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:00:14 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,19 @@ int main(int argc, char *argv[])
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		for(int i = 1; i < argc; i++)
+	for(int i = 1; i < argc; i++)
+	{
+		for(int j = 0; argv[i][j] != '\0'; j++)
 		{
-			for(int j = 0; argv[i][j] != '\0'; j++)
+			if((argv[i][j] >= 97 && argv[i][j] <= 122 ))
 			{
 				std::cout << (char)std::toupper(argv[i][j]);
 			}
+			else
+				std::cout << argv[i][j];
 		}
-		std::cout << std::endl;
+	}
+	std::cout << std::endl;
 	}
 	return 0;
 }
