bdb05a3cd306bca0fab9303917a1720ad7309ddd
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2024-09-02 18:32:43 +0300

    refactoring some of functions

--- M00/ex01/Contact.hpp
+++ M00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 11:16:49 by aheinane          #+#    #+#             */
-/*   Updated: 2024/08/29 14:32:50 by aheinane         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:29:55 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,54 +25,19 @@ class Contact
 		std::string darkestSecret;
 		bool at_least_one;
 	public:
-		void set_fn(std::string s_firstName)
-		{
-			this->firstName = s_firstName;
-		}
-		void set_ln(std::string s_lastName)
-		{
-			this->lastName = s_lastName;
-		}
-		void set_nn(std::string s_nickName)
-		{
-			this->nickname = s_nickName;
-		}
-		void set_pn(unsigned s_phoneNumber)
-		{
-			this->phoneNumber = s_phoneNumber;
-		}
-		void set_ds(std::string s_darkestSecret)
-		{
-			this->darkestSecret = s_darkestSecret;
-		}
-		void set_at_least_one(unsigned at_least_one)
-		{
-			this->at_least_one = at_least_one;
-		}
-		std::string get_fn()
-		{
-			return(this->firstName);
-		}
-		std::string get_ln()
-		{
-			return(this->lastName);
-		}
-		std::string get_nn()
-		{
-			return(this->nickname);
-		}
-		unsigned get_pn()
-		{
-			return(this->phoneNumber);
-		}
-		std::string get_ds()
-		{
-			return(this->darkestSecret);
-		}
-		unsigned get_at_least_one()
-		{
-			return(this->at_least_one);
-		}
+		void set_fn(std::string s_firstName);
+		void set_ln(std::string s_lastName);
+		void set_nn(std::string s_nickName);
+		void set_pn(unsigned s_phoneNumber);
+		void set_ds(std::string s_darkestSecret);
+		void set_at_least_one(unsigned at_least_one);
+
+		std::string get_fn();
+		std::string get_ln();
+		std::string get_nn();
+		unsigned get_pn();
+		std::string get_ds();
+		unsigned get_at_least_one();
 };
 
 #endif
--- M01/ex03/Weapon.hpp
+++ M01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 10:40:52 by aheinane          #+#    #+#             */
-/*   Updated: 2024/08/27 14:17:34 by aheinane         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:32:03 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,8 @@ class Weapon
 		{
 			this->type = type;
 		};
-		void setType(std:: string type)
-		{
-			this->type = type;
-		}
-		std::string getType()
-		{
-			return(this->type);
-		}
+		void setType(std:: string type);
+		std::string getType();
 };
 
 
