a29f31feb8fa66364afaa5044a6abeceff72acd9
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2024-11-05 14:59:23 +0200

    M05 ex03 starting

--- M05/ex02/main.cpp
+++ M05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:38:31 by aheinane          #+#    #+#             */
-/*   Updated: 2024/11/05 14:09:29 by aheinane         ###   ########.fr       */
+/*   Updated: 2024/11/05 14:29:21 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,35 +86,35 @@ int main ()
 
 
 	////Shrubbery
-	try
-	{
-		ShrubberyCreationForm one("FIRST");
+	// try
+	// {
+	// 	ShrubberyCreationForm one("FIRST");
 		
-		std::cout << "\033[31m" << "\nPresindetial Form one created with default name" <<"\033[0m" << std::endl;
-		std::cout << one << std::endl;
+	// 	std::cout << "\033[31m" << "\nPresindetial Form one created with default name" <<"\033[0m" << std::endl;
+	// 	std::cout << one << std::endl;
 
-		std::cout << "\033[31m" << "\nBureacrat kuku created with name kuku" <<"\033[0m" << std::endl;
-		Bureaucrat kuku ("kuku", 25);
+	// 	std::cout << "\033[31m" << "\nBureacrat kuku created with name kuku" <<"\033[0m" << std::endl;
+	// 	Bureaucrat kuku ("kuku", 25);
 		
-		std::cout << "\033[31m" << "\nForm is not signed if bur. grade is more than 25" <<"\033[0m" << std::endl;
-		one.beSigned(kuku);
-		one.execute(kuku);
+	// 	std::cout << "\033[31m" << "\nForm is not signed if bur. grade is more than 25" <<"\033[0m" << std::endl;
+	// 	one.beSigned(kuku);
+	// 	one.execute(kuku);
 
 		
-		std::cout << "\033[34m" << "\nDESTRUCTORS" <<"\033[0m" << std::endl;
-	}
-	 catch(Bureaucrat::GradeTooHighException& e){
-		std::cout << e.what() << std::endl; /// SHOULD CATCH ERROR TOO HIGH
-	}
-	catch(Bureaucrat::GradeTooLowException& e){
-		std::cout << e.what() << std::endl; /// SHOULD CATCH ERROR TOO LOW
-	}
-	catch(AForm::GradeTooHighException& e){
-		std::cout << e.what() << std::endl; /// SHOULD CATCH ERROR TOO HIGH
-	}
-	catch(AForm::GradeTooLowException& e){
-		std::cout << e.what() << std::endl; /// SHOULD CATCH ERROR TOO LOW 
-	}
+	// 	std::cout << "\033[34m" << "\nDESTRUCTORS" <<"\033[0m" << std::endl;
+	// }
+	//  catch(Bureaucrat::GradeTooHighException& e){
+	// 	std::cout << e.what() << std::endl; /// SHOULD CATCH ERROR TOO HIGH
+	// }
+	// catch(Bureaucrat::GradeTooLowException& e){
+	// 	std::cout << e.what() << std::endl; /// SHOULD CATCH ERROR TOO LOW
+	// }
+	// catch(AForm::GradeTooHighException& e){
+	// 	std::cout << e.what() << std::endl; /// SHOULD CATCH ERROR TOO HIGH
+	// }
+	// catch(AForm::GradeTooLowException& e){
+	// 	std::cout << e.what() << std::endl; /// SHOULD CATCH ERROR TOO LOW 
+	// }
 	
 	return(0);
 }
