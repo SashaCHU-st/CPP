cd17daedc000da37fdf09f8e362d620f3fee8d39
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2024-11-22 18:32:04 +0200

    m08...doont know how to start, will contunue later

--- M08/ex00/easyfind.hpp
+++ M08/ex00/easyfind.hpp
@@ -2,10 +2,20 @@
 
 #include <iostream>
 #include <vector>
+#include <exception>
 
 
 template <typename T>
 T easyfind(T x, int y)
 {
+   find_if(x.begin(), x.end, y)
+
+   if()
+   {
+        std::cout<<"not found"<<std::endl;
+        return;
+   }
+    std::cout<<y<<" found in container"<<std::endl;
+
     
 }
--- M08/ex00/main.cpp
+++ M08/ex00/main.cpp
@@ -2,5 +2,16 @@
 
 int main ()
 {
-    std::vector<int> T = {1, 2, 3};
+    std::vector<int> vec = {1,2,3,4,5, 6};
+
+
+    
+    std::vector<int>::iterator v = vec.begin();
+    while( v != vec.end()) {
+      std::cout << "value of v = " << *v << std::endl;
+      v++;
+   }
+
+    easyfind(vec, 3);
+
 }
