26aecfe42d55c47e37c654afea5a1ad0ba6a0c18
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2025-02-11 10:36:04 +0200

    ex02 done

--- M08/ex02/MutantStack.hpp
+++ M08/ex02/MutantStack.hpp
@@ -4,8 +4,33 @@
 #include <vector>
 #include <stack>
 
-template <typename T> 
-class MutantStack{
+template <typename T, typename Container = std::deque<T>> 
+class MutantStack: public std::stack <T, Container>{
     public:
+        MutantStack():std:: stack<T, Container>(){};
+        ~MutantStack(){}
+
+        typedef typename Container::iterator iterator;
+
+        MutantStack(const MutantStack& copy)
+        {
+            *this = copy;
+        }
+
+        MutantStack& operator= (const MutantStack& copy)
+        {
+            if(this!= &copy)
+                std::stack<T, Container>::operator=(copy)
+            return(*this);
+        };
+
+        iterator begin()
+        {
+            return(this->c.begin());
+        }
+        iterator end()
+        {
+            return(this->c.end());
+        }
 
 };
--- M08/ex02/main.cpp
+++ M08/ex02/main.cpp
@@ -4,23 +4,38 @@ int main()
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
+    std::cout << mstack.top() << std::endl;// print 17 LIFO
+    mstack.pop();// deleted 17 recent entered element
+    std::cout << mstack.top() << std::endl;// print 5 the element that left
+    std::cout << mstack.size() << std::endl;// print 1 because 1 has been deleted
+    mstack.push(3);// 3 5
+    mstack.push(5);// 5 3 5
+    mstack.push(737); // 737 5 3 5
     //[...]
-    mstack.push(0);
+    mstack.push(0); // 0 737 5 3 5
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
     --it;
     while (it != ite)
     {
-    std::cout << *it << std::endl;
+    std::cout << "IT "<< *it << std::endl;
     ++it;
     }
-    std::stack<int> s(mstack);
+    std::stack<int> s(mstack);// coping stack
+
+    while (!mstack.empty())
+    {
+        std::cout << "mstack: " << mstack.top() << std::endl;
+        mstack.pop();
+    }
+
+    while (!s.empty())
+    {
+        std::cout << "s: " << s.top() << std::endl;
+        s.pop();
+    }
+
+
     return 0;
 }
