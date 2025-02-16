2d78536e0a9b2d2b162b06dfbc1c7404d1a930b3
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2025-02-16 15:58:03 +0200

    ex01 ready, support negative numbers

--- M09/ex01/RPN.cpp
+++ M09/ex01/RPN.cpp
@@ -15,51 +15,56 @@ int RPN::polishCalculation(std::string argument)
     std::stringstream ss(argument);
     std::string token;
 
-    while (ss >> token)// read separated whitespace word/token
+    while (ss >> token) // read separated whitespace word/token
     {
-        if (isdigit(token[0])) // beacuse we want only from 1-9, not 12
+        //         if (isdigit(token[0])) // beacuse we want only from 1-9,niot 12
+//         {
+//             polishNumbers.push(std::stoi(token));
+//         }
+        try 
         {
-            polishNumbers.push(std::stoi(token));
+            int num = std::stoi(token); // Convert string to int
+            polishNumbers.push(num);
         }
-        else if (token == "+" || token == "-" || token == "*" || token == "/")
+        catch (const std::exception& e) 
         {
-            if (polishNumbers.size() < 2)
+            if (token == "+" || token == "-" || token == "*" || token == "/")// If ni number the maybe it is =/-///*
             {
-                std::cerr << "Usage ./RPN \"1 2 + 3 -\""<<std::endl;;
-                return 1;
-            }
-
-            int number1 = polishNumbers.top();
-            polishNumbers.pop();
-            int number2 = polishNumbers.top();
-            polishNumbers.pop();
-
-            if (token == "+")
-                polishNumbers.push(number1 + number2);
-            else if (token == "-")
-                polishNumbers.push(number1 - number2);
-            else if (token == "*")
-                polishNumbers.push(number1 * number2);
-            else if (token == "/")
-            {
-                if (number2 == 0)
+                if (polishNumbers.size() < 2)
                 {
-                    std::cerr << "Error: Division by zero" <<std::endl;
+                    std::cerr << "Error" << std::endl;
                     return 1;
                 }
-                polishNumbers.push(number1 / number2);
+                int number2 = polishNumbers.top();
+                polishNumbers.pop();
+                int number1 = polishNumbers.top();
+                polishNumbers.pop();
+                if (token == "+")
+                    polishNumbers.push(number1 + number2);
+                else if (token == "-")
+                    polishNumbers.push(number1 - number2);
+                else if (token == "*")
+                    polishNumbers.push(number1 * number2);
+                else if (token == "/")
+                {
+                    if (number2 == 0)
+                    {
+                        std::cerr << "Error" << std::endl;
+                        return 1;
+                    }
+                    polishNumbers.push(number1 / number2);
+                }
+            }
+            else
+            {
+                std::cerr << "Error" << std::endl;
+                return 1;
             }
         }
-        else
-        {
-            std::cerr << "Error "<< std::endl;
-            return 1;
-        }
     }
-
     if (polishNumbers.size() != 1)
     {
-        std::cerr << "Error: Invalid RPN expression (too many numbers left)"<<std::endl;
+        std::cerr << "Error" << std::endl;
         return 1;
     }
 
