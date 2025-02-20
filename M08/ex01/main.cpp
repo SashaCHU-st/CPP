960c202a8dc892c9a56ee97c73186fae79031b42
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2025-02-20 10:29:41 +0200

    m08 all good

--- M08/ex01/main.cpp
+++ M08/ex01/main.cpp
@@ -13,10 +13,10 @@ int main()
 
 
     Span sp2;
-    std::vector<int> _numbers = { 1, 2, 3, 4, 5 };
+    std::vector<int> _numbers = { 1, 2, 3, 4 };
     std::vector<int> _numbers2 = { 1, 2, 3, 4, 5 };
     unsigned int hundred = 100;
-    std::vector<int> _numbers3(hundred, 7);
+    std::vector<int> _numbers3(hundred, 7);//vector size of 100 filled with 7
     sp2.addNumbers(_numbers.begin(), _numbers.end());
     sp2.addNumbers(_numbers2.begin(), _numbers2.end());
     sp2.addNumbers(_numbers3.begin(), _numbers3.end());
