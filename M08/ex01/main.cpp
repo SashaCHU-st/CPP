61d6dc32900ff5d3dbddfcd4e45da9d586d96a3a
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2025-02-21 13:06:26 +0200

    reade m08

--- M08/ex01/main.cpp
+++ M08/ex01/main.cpp
@@ -31,6 +31,21 @@ int main()
     catch (const std::out_of_range& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
+     try{
+        Span sp2(140);
+        std::vector<int> _numbers = { 1, 2, 3, 4 };
+        std::vector<int> _numbers2 = { 1, 2, 3, 4, 5 };
+        unsigned int hundred = 100;
+        std::vector<int> _numbers3(hundred, 7);//vector size of 100 filled with 7
+        sp2.addNumbers(_numbers.begin(), _numbers.end());
+        sp2.addNumbers(_numbers2.begin(), _numbers2.end());
+        sp2.addNumbers(_numbers3.begin(), _numbers3.end());
+
+        sp2.print();
+    }
+    catch (const std::out_of_range& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 
     return(0);
 }
